42e2581b5333bf6eb14a4b1c88ae2662097c64fc
聪聪 <178666380@qq.com>
2017-06-27 14:15:35 +0800

    修复Rcon发送命令可能报错的问题

--- src/main/java/org/bukkit/craftbukkit/command/CraftSimpleCommandMap.java
+++ src/main/java/org/bukkit/craftbukkit/command/CraftSimpleCommandMap.java
@@ -5,12 +5,14 @@ import static org.bukkit.util.Java15Compat.Arrays_copyOfRange;
 import java.util.regex.Pattern;
 
 import net.minecraft.command.ICommandSender;
+import net.minecraft.network.rcon.RConConsoleSource;
 
 import org.bukkit.Server;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandException;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.command.RemoteConsoleCommandSender;
 import org.bukkit.command.SimpleCommandMap;
 import org.bukkit.craftbukkit.entity.CraftPlayer;
 
@@ -49,6 +51,9 @@ public class CraftSimpleCommandMap extends SimpleCommandMap {
                 if (sender instanceof ConsoleCommandSender)
                 {
                     FMLCommonHandler.instance().getMinecraftServerInstance().getCommandManager().executeCommand(this.vanillaConsoleSender, commandLine);
+                }
+                else if (sender instanceof RemoteConsoleCommandSender){
+                    FMLCommonHandler.instance().getMinecraftServerInstance().getCommandManager().executeCommand(RConConsoleSource.instance, commandLine);
                 }
                 else FMLCommonHandler.instance().getMinecraftServerInstance().getCommandManager().executeCommand(((CraftPlayer)sender).getHandle(), commandLine);
             }
