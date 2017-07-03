d486cf7105f1f3fc37fba91171253b9442c521c3
xjboss <i@xjboss.net>
2017-07-03 19:49:24 +0800

    ........

--- src/main/java/kcauldronx/events/ModInventoryMoveToPlayerEvent.java
+++ src/main/java/kcauldronx/events/ModInventoryMoveToPlayerEvent.java
@@ -6,13 +6,12 @@ import org.bukkit.craftbukkit.inventory.CraftInventory;
 import org.bukkit.craftbukkit.inventory.CraftInventoryPlayer;
 import org.bukkit.craftbukkit.inventory.CraftItemStack;
 import org.bukkit.event.inventory.InventoryMoveItemEvent;
-import org.bukkit.inventory.PlayerInventory;
 
 /**
  * Created by xjboss on 2017/7/3.
  */
 public class ModInventoryMoveToPlayerEvent extends InventoryMoveItemEvent {
-    public ModInventoryMoveToPlayerEvent(IInventory sourceInventory, net.minecraft.item.ItemStack itemStack, PlayerInventory destinationInventory, boolean didSourceInitiate) {
+    public ModInventoryMoveToPlayerEvent(IInventory sourceInventory, net.minecraft.item.ItemStack itemStack, InventoryPlayer destinationInventory, boolean didSourceInitiate) {
         super(new CraftInventory(sourceInventory), CraftItemStack.asCraftMirror(itemStack), new CraftInventoryPlayer((InventoryPlayer)destinationInventory), didSourceInitiate);
     }
 }
