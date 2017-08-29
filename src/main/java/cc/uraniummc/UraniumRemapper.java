cdbaf8d813c90244260a560095d1e99383cf450a
聪聪 <178666380@qq.com>
2017-08-30 02:43:07 +0800

    当插件jar中包含某个类时,不对该类remap

--- src/main/java/cc/uraniummc/UraniumRemapper.java
+++ src/main/java/cc/uraniummc/UraniumRemapper.java
@@ -1,19 +1,33 @@
 package cc.uraniummc;
 
+import org.bukkit.plugin.java.PluginClassLoader;
+
 import net.md_5.specialsource.JarMapping;
 import net.md_5.specialsource.JarRemapper;
 
-public class UraniumRemapper extends JarRemapper {
-    public UraniumRemapper(JarMapping jarMapping) {
+public class UraniumRemapper extends JarRemapper{
+
+    protected final PluginClassLoader mLoader;
+
+    public UraniumRemapper(JarMapping jarMapping,PluginClassLoader pPluginClassLoader){
         super(jarMapping);
+
+        this.mLoader=pPluginClassLoader;
     }
 
     @Override
-    public String mapSignature(String signature, boolean typeSignature) {
-        try {
-            return super.mapSignature(signature, typeSignature);
-        } catch (Exception e) {
+    public String mapSignature(String signature,boolean typeSignature){
+        try{
+            return super.mapSignature(signature,typeSignature);
+        }catch(Exception e){
             return signature;
         }
+    }
+
+    @Override
+    public String map(String typeName){
+        // not remap class that exist in jar file
+        // this mean plugin use it's owner lib 
+        return this.mLoader.findResource(typeName.replace('.','/').concat(".class"))!=null?typeName:super.map(typeName);
     }
 }
