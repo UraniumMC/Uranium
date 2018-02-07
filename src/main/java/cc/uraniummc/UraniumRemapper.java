7129b49a2c0d4411555b036b5b4474ee9a19ff8e
聪聪 <178666380@qq.com>
2018-02-07 19:27:03 +0800

    remap代码改进

--- src/main/java/cc/uraniummc/UraniumRemapper.java
+++ src/main/java/cc/uraniummc/UraniumRemapper.java
@@ -1,36 +1,37 @@
 package cc.uraniummc;
 
-import cc.uraniummc.util.NMSClassUtil;
-import com.sun.org.apache.bcel.internal.classfile.LocalVariable;
-import lombok.Setter;
-import lombok.experimental.var;
-import net.md_5.specialsource.RemappingClassAdapter;
-import net.md_5.specialsource.SpecialSource;
-import net.md_5.specialsource.repo.ClassRepo;
-import org.bukkit.plugin.java.PluginClassLoader;
-
-import net.md_5.specialsource.JarMapping;
-import net.md_5.specialsource.JarRemapper;
-import org.objectweb.asm.*;
-import org.objectweb.asm.commons.GeneratorAdapter;
-import org.objectweb.asm.commons.Method;
-import org.objectweb.asm.tree.*;
-
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.*;
-
 import static org.objectweb.asm.ClassWriter.COMPUTE_MAXS;
 
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.bukkit.plugin.java.PluginClassLoader;
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.Type;
+import org.objectweb.asm.tree.AbstractInsnNode;
+import org.objectweb.asm.tree.ClassNode;
+import org.objectweb.asm.tree.LdcInsnNode;
+import org.objectweb.asm.tree.MethodInsnNode;
+import org.objectweb.asm.tree.MethodNode;
+
+import cc.uraniummc.util.NMSClassUtil;
+import lombok.Setter;
+import net.md_5.specialsource.JarMapping;
+import net.md_5.specialsource.JarRemapper;
+import net.md_5.specialsource.repo.ClassRepo;
+
 public class UraniumRemapper extends JarRemapper implements Opcodes{
+
     protected final PluginClassLoader mLoader;
-    private int writerFlags = COMPUTE_MAXS;
-    private int readerFlags = 0;
+    private int writerFlags=COMPUTE_MAXS;
+    private int readerFlags=0;
     @Setter
     private boolean debug=false;
 
     private boolean RemapReflection=false;
+
     public UraniumRemapper(JarMapping jarMapping,PluginClassLoader pPluginClassLoader){
         super(jarMapping);
 
@@ -57,164 +58,54 @@ public class UraniumRemapper extends JarRemapper implements Opcodes{
         // this mean plugin use it's owner lib 
         return this.mLoader.findResource(typeName.replace('.','/').concat(".class"))!=null?typeName:super.map(typeName);
     }
+
     /**
      * Remap an individual class given an InputStream to its bytecode
      */
-    public byte[] remapClassFile(InputStream is, ClassRepo repo) throws IOException {
-        return RemapReflection?remapClassFile(new ClassReader(super.remapClassFile(is, repo))):super.remapClassFile(is,repo);
+    public byte[] remapClassFile(InputStream is,ClassRepo repo) throws IOException{
+        return RemapReflection?remapClassFile(new ClassReader(super.remapClassFile(is,repo))):super.remapClassFile(is,repo);
     }
 
-    public byte[] remapClassFile(byte[] in, ClassRepo repo) {
-        return RemapReflection?remapClassFile(new ClassReader(super.remapClassFile(in, repo))):super.remapClassFile(in,repo);
+    public byte[] remapClassFile(byte[] in,ClassRepo repo){
+        return RemapReflection?remapClassFile(new ClassReader(super.remapClassFile(in,repo))):super.remapClassFile(in,repo);
     }
 
-    private void logR(String message) {
-        if (debug) {
-            System.out.println("[ReflectionRemapper] " + message);
+    private void logR(String message){
+        if(debug){
+            System.out.println("[ReflectionRemapper] "+message);
         }
     }
-    private int FindMethodStartPos(int start,InsnList list,MethodInsnNode method){
-        Type mt=Type.getMethodType(method.desc);
-        AbstractInsnNode new_pos=null;
-        Type[] types=mt.getArgumentTypes();
-        if(types!=null) {
-            for (int ti = types.length - 1; ti >= 0; ti--) {
-                Type ft = types[ti];
-                if (ft.getSort() == Type.ARRAY) {
-                    if ((new_pos = list.get(start - 1)).getOpcode() != AALOAD) {
-                        do {
-                            new_pos = list.get(--start);
-                            if (new_pos.getOpcode() == ANEWARRAY) {
-                                String ndec=ft.getDescriptor();
-                                ndec=ndec.substring(2);
-                                if(ndec.equals(((TypeInsnNode) new_pos).desc+";")){
-                                    new_pos = list.get(--start);
-                                    break;
-                            }
-                            } else if (new_pos instanceof MethodInsnNode) {
-                                    start = FindMethodStartPos(start, list, (MethodInsnNode) new_pos);
-                            }
-                        } while (true);
-                    }
-                } else {
-                    boolean bre=false;
-                    do {
-                        new_pos = list.get(--start);
-                        if (new_pos instanceof MethodInsnNode) {
-                            start = FindMethodStartPos(start, list, (MethodInsnNode) new_pos);
-                            break;
-                        }
-                        switch (new_pos.getOpcode()) {
-                            case INVOKEDYNAMIC:
-                            case LDC:
-                            case ICONST_0:
-                            case ICONST_1:
-                            case ICONST_2:
-                            case ICONST_3:
-                            case ICONST_4:
-                            case ICONST_5:
-                            case LCONST_0:
-                            case LCONST_1:
-                            case FCONST_0:
-                            case FCONST_1:
-                            case FCONST_2:
-                            case DCONST_0:
-                            case DCONST_1:
-                            case BIPUSH:
-                            case SIPUSH:
-                            case ILOAD:
-                            case LLOAD:
-                            case FLOAD:
-                            case DLOAD:
-                            case ALOAD:
-                                bre=true;
-                        }
-                    }
-                    while (!(new_pos instanceof IntInsnNode || new_pos instanceof VarInsnNode || new_pos instanceof LdcInsnNode || bre));
-                }
-            }
-        }
-        if(method.getOpcode()==INVOKEVIRTUAL||method.getOpcode()==INVOKESPECIAL||method.getOpcode()==INVOKEINTERFACE)--start;
-        return start;
-    }
+
     @SuppressWarnings("unchecked")
-    private byte[] remapClassFile(ClassReader reader) {
-        ClassNode cn=new ClassNode();
-        reader.accept(cn, ClassReader.EXPAND_FRAMES);
-        HashSet<Object> varTypes=new HashSet();
-        for(int i=0;i<cn.methods.size();i++){
-            MethodNode nm=cn.methods.get(i);
-            ArrayList vt=new ArrayList();
-            vt.addAll(varTypes);
-            if(nm.localVariables!=null) {
-                for (int lv = 0; lv < nm.localVariables.size(); lv++) {
-                    LocalVariableNode lvar = nm.localVariables.get(lv);
-                    if (lvar.desc.equals("Ljava/lang/Class;")) vt.add(lvar.index);
-                }
-            }
-            AbstractInsnNode loadedclass_pos=null;
-            for(int inst=0;inst<nm.instructions.size();inst++){
-                AbstractInsnNode in=nm.instructions.get(inst);
-                if(in instanceof VarInsnNode){
-                    if(vt.contains(((VarInsnNode) in).var)&&in.getOpcode()==ALOAD){
-                        loadedclass_pos=in;
+    private byte[] remapClassFile(ClassReader pCReader){
+        ClassNode tCNode=new ClassNode();
+        pCReader.accept(tCNode,ClassReader.EXPAND_FRAMES);
+        for(int i=0;i<tCNode.methods.size();i++){
+            MethodNode tMNode=tCNode.methods.get(i);
+            for(int j=0;j<tMNode.instructions.size();j++){
+                AbstractInsnNode tInsnNode=tMNode.instructions.get(j);
+                if(tInsnNode instanceof MethodInsnNode&&(tInsnNode.getOpcode()>=INVOKEVIRTUAL&&tInsnNode.getOpcode()<INVOKEDYNAMIC)){
+                    MethodInsnNode mi=(MethodInsnNode)tInsnNode;
+                    if(mi.owner.equals("java/lang/Class")&&(mi.name.equals("getDeclaredMethod")||
+                            mi.name.equals("getMethod")||
+                            mi.name.equals("getField")||
+                            mi.name.equals("getDeclaredField"))){
+                        mi.setOpcode(INVOKESTATIC);
+                        mi.owner=Type.getType(NMSClassUtil.class).getInternalName();
+                        mi.desc="(Ljava/lang/Class;"+(mi.desc.substring(1));
+                        logR("Remapped Class."+mi.name);
+                    }else if(mi.getOpcode()==INVOKESTATIC&&mi.owner.equals("java/lang/Class")&&mi.name.equals("forName")){
+                        tMNode.instructions.insertBefore(mi,new LdcInsnNode(Type.getObjectType(mLoader.getDescription().getMain().replace(".","/"))));
+                        mi.owner=Type.getType(NMSClassUtil.class).getInternalName();
+                        int tIndex=mi.desc.indexOf(')');
+                        mi.desc=mi.desc.substring(0,tIndex)+"Ljava/lang/Class;"+(mi.desc.substring(tIndex));
+                        logR("Remapped Class.forName");
                     }
-                }else if(in instanceof MethodInsnNode){
-                    MethodInsnNode mi=(MethodInsnNode) in;
-                    if(in.getOpcode()>=INVOKEVIRTUAL&&in.getOpcode()<INVOKEDYNAMIC){
-                        if(loadedclass_pos!=null&&mi.owner.equals("java/lang/Class")&&(mi.name.equals("getDeclaredMethod") || mi.name.equals("getMethod") || mi.name.equals("getField") || mi.name.equals("getDeclaredField"))){
-                            AbstractInsnNode new_pos=nm.instructions.get(FindMethodStartPos(inst,nm.instructions,mi));
-                            nm.instructions.insertBefore(loadedclass_pos,new TypeInsnNode(NEW,Type.getType(NMSClassUtil.class).getInternalName()));
-                            nm.instructions.insertBefore(loadedclass_pos,new InsnNode(DUP));
-                            MethodInsnNode newNMS=new MethodInsnNode(INVOKESPECIAL, Type.getType(NMSClassUtil.class).getInternalName(), "<init>", "(Ljava/lang/Class;Ljava/lang/Class;)V",false);
-                            nm.instructions.insert(new_pos,newNMS);
-                            nm.instructions.insertBefore(newNMS,new LdcInsnNode(Type.getObjectType(mLoader.getDescription().getMain().replace(".", "/"))));
-                            mi.setOpcode(INVOKEVIRTUAL);
-                            mi.owner=Type.getType(NMSClassUtil.class).getInternalName();
-                            loadedclass_pos=null;
-                            inst+=4;
-                            logR("Remapped Class."+mi.name);
-                        }else if(mi.getOpcode()==INVOKESTATIC&&mi.owner.equals("java/lang/Class")&&mi.name.equals("forName")){
-                            if(mi.desc.equals("(Ljava/lang/String;)Ljava/lang/Class;")) {
-                                nm.instructions.insertBefore(mi,new LdcInsnNode(Type.getObjectType(mLoader.getDescription().getMain().replace(".", "/"))));
-                                mi.owner=Type.getType(NMSClassUtil.class).getInternalName();
-                                mi.desc="(Ljava/lang/String;Ljava/lang/Class;)Ljava/lang/Class;";
-                                inst+=1;
-                            }else{
-                                nm.instructions.insertBefore(mi,new LdcInsnNode(Type.getObjectType(mLoader.getDescription().getMain().replace(".", "/"))));
-                                mi.owner=Type.getType(NMSClassUtil.class).getInternalName();
-                                mi.desc="(Ljava/lang/String;ZLjava/lang/ClassLoader;Ljava/lang/Class;)Ljava/lang/Class;";
-                                inst+=1;
-                            }
-                            logR("Remapped Class.forName");
-                        }else if(mi.desc.endsWith("Ljava/lang/Class;")&&loadedclass_pos==null){
-                            loadedclass_pos=nm.instructions.get(FindMethodStartPos(inst,nm.instructions,mi));
-                        }
-                    }
-                }else if(in instanceof LabelNode){
-                    loadedclass_pos=null;
-                }else if(in instanceof FieldInsnNode&&loadedclass_pos==null){
-                    if(((FieldInsnNode) in).desc.equals("Ljava/lang/Class;")){
-                        if(in.getOpcode()==GETFIELD){
-                            loadedclass_pos=nm.instructions.get(inst-1);
-                            if(loadedclass_pos instanceof MethodInsnNode){
-                                loadedclass_pos=nm.instructions.get(FindMethodStartPos(inst-1,nm.instructions,(MethodInsnNode)loadedclass_pos));
-                            }
-                        }else if(in.getOpcode()==GETSTATIC) {
-                            loadedclass_pos = in;
-                        }
-                        vt.add(((FieldInsnNode) in).owner+"."+((FieldInsnNode) in).name);
-                    }
-                }
-            }
-            for(Object v:vt){
-                if(v instanceof String){
-                    varTypes.add(v);
                 }
             }
         }
         ClassWriter cw=new ClassWriter(ClassWriter.COMPUTE_MAXS);
-        cn.accept(cw);
+        tCNode.accept(cw);
         return cw.toByteArray();
     }
 
--- src/main/java/cc/uraniummc/util/NMSClassUtil.java
+++ src/main/java/cc/uraniummc/util/NMSClassUtil.java
@@ -1,88 +1,86 @@
 package cc.uraniummc.util;
 
-import net.minecraft.server.MinecraftServer;
-import org.bukkit.Bukkit;
-import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.plugin.java.PluginClassLoader;
-import sun.reflect.CallerSensitive;
-
 import java.lang.reflect.Field;
-import java.lang.reflect.Member;
 import java.lang.reflect.Method;
 import java.net.URLClassLoader;
 
-public class NMSClassUtil {
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.PluginClassLoader;
+
+import net.minecraft.server.MinecraftServer;
+import sun.reflect.CallerSensitive;
+
+public class NMSClassUtil{
+
     private final Class<?> mClass;
     private final URLClassLoader mCL;
+
     public NMSClassUtil(Class<?> pClass,Class<?> pPClass){
         this.mClass=pClass;
-        mCL=(URLClassLoader) pPClass.getClassLoader();
+        mCL=(URLClassLoader)pPClass.getClassLoader();
     }
+
     public static String BgetName(Class<?> pPClass){
-        URLClassLoader mCL=(URLClassLoader) pPClass.getClassLoader();
+        URLClassLoader mCL=(URLClassLoader)pPClass.getClassLoader();
         if(mCL instanceof PluginClassLoader){
             String sname;
-            sname= MinecraftServer.uraniumConfig.getString("plugin-settings." + ((PluginClassLoader) mCL).getDescription().getName()+ ".custom-server-name", "");
+            sname=MinecraftServer.uraniumConfig.getString("plugin-settings."+((PluginClassLoader)mCL).getDescription().getName()+".custom-server-name","");
             if(sname!=null&&!sname.isEmpty()){
                 return sname;
             }
         }
         return Bukkit.getServerName();
     }
+
     @CallerSensitive
-    public static Class<?> forName(String className,Class<?> pPClass)
-            throws ClassNotFoundException {
-        URLClassLoader mCL=(URLClassLoader) pPClass.getClassLoader();
-        if(mCL instanceof PluginClassLoader){
-            return Class.forName(((PluginClassLoader) mCL).umcl.remapClass(className).replace("/","."));
-        }
-        return Class.forName(className);
-    }
-    @CallerSensitive
-    public static Class<?> forName(String className,boolean init,ClassLoader loader,Class<?> pPClass)
-            throws ClassNotFoundException {
-        URLClassLoader mCL=(URLClassLoader) pPClass.getClassLoader();
-        if(mCL instanceof PluginClassLoader){
-            return Class.forName(((PluginClassLoader) mCL).umcl.remapClass(className).replace("/","."),init,loader);
-        }
-        return Class.forName(className,init,loader);
-    }
-    @CallerSensitive
-    public Method getDeclaredMethod(String name, Class<?>... parameterTypes)
-            throws NoSuchMethodException, SecurityException {
-        if(mCL instanceof PluginClassLoader) {
-            String newName = ((PluginClassLoader) mCL).umcl.remapMethod(mClass,name,parameterTypes);
-            return mClass.getDeclaredMethod(newName,parameterTypes);
-        }
-        return mClass.getDeclaredMethod(name,parameterTypes);
+    public static Class<?> forName(String pClassName,Class<?> pClass) throws ClassNotFoundException{
+        return forName0(pClass,pClassName,true,pClass.getClassLoader());
     }
 
     @CallerSensitive
-    public Method getMethod(String name, Class<?>... parameterTypes)
-            throws NoSuchMethodException, SecurityException {
-        if(mCL instanceof PluginClassLoader) {
-            String newName = ((PluginClassLoader) mCL).umcl.remapMethod(mClass,name,parameterTypes);
-            return mClass.getMethod(newName,parameterTypes);
+    public static Class<?> forName(String pClassName,boolean pInit,ClassLoader pLoader,Class<?> pClass) throws ClassNotFoundException{
+        return forName0(pClass,pClassName,pInit,pLoader);
+    }
+    
+    @CallerSensitive
+    private static Class<?> forName0(Class<?> pClass,String pClassName,boolean pInit,ClassLoader pLoader) throws ClassNotFoundException{
+        URLClassLoader mCL=(URLClassLoader)pClass.getClassLoader();
+        if(mCL instanceof PluginClassLoader){
+            pClassName=((PluginClassLoader)mCL).umcl.remapClass(pClassName).replace("/",".");
         }
-        return mClass.getMethod(name,parameterTypes);
+        return Class.forName(pClassName,pInit,pLoader==null?pClass.getClassLoader():pLoader);
     }
 
     @CallerSensitive
-    public Field getField(String name)
-            throws NoSuchFieldException, SecurityException {
-        if(mCL instanceof PluginClassLoader) {
-            String newName = ((PluginClassLoader) mCL).umcl.remapField(mClass,name);
-            return mClass.getField(newName);
+    public static Method getMethod(Class<?> pClazz,String pMethodName,Class<?>...pParamTypes) throws NoSuchMethodException,SecurityException{
+        if(pClazz.getClassLoader() instanceof PluginClassLoader){
+            pMethodName=((PluginClassLoader)pClazz.getClassLoader()).umcl.remapMethod(pClazz,pMethodName,pParamTypes);
         }
-        return mClass.getField(name);
+        return pClazz.getMethod(pMethodName,pParamTypes);
     }
+
     @CallerSensitive
-    public Field getDeclaredField(String name)
-            throws NoSuchFieldException, SecurityException {
-        if(mCL instanceof PluginClassLoader) {
-            String newName = ((PluginClassLoader) mCL).umcl.remapField(mClass,name);
-            return mClass.getDeclaredField(newName);
+    public static Method getDeclaredMethod(Class<?> pClazz,String pMethodName,Class<?>...pParamTypes) throws NoSuchMethodException,SecurityException{
+        if(pClazz.getClassLoader() instanceof PluginClassLoader){
+            pMethodName=((PluginClassLoader)pClazz.getClassLoader()).umcl.remapMethod(pClazz,pMethodName,pParamTypes);
         }
-        return mClass.getDeclaredField(name);
+        return pClazz.getDeclaredMethod(pMethodName,pParamTypes);
     }
+
+    @CallerSensitive
+    public static Field getField(Class<?> pClazz,String pFieldName) throws NoSuchFieldException,SecurityException{
+        if(pClazz.getClassLoader() instanceof PluginClassLoader){
+            pFieldName=((PluginClassLoader)pClazz.getClassLoader()).umcl.remapField(pClazz,pFieldName);
+        }
+        return pClazz.getField(pFieldName);
+    }
+
+    @CallerSensitive
+    public static Field getDeclaredField(Class<?> pClazz,String pFieldName) throws NoSuchFieldException,SecurityException{
+        if(pClazz.getClassLoader() instanceof PluginClassLoader){
+            pFieldName=((PluginClassLoader)pClazz.getClassLoader()).umcl.remapField(pClazz,pFieldName);
+        }
+        return pClazz.getDeclaredField(pFieldName);
+    }
+
 }
