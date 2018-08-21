57f98bf52cfa8a50267d21c51b5c9474cad7477e
xjboss <i@xjboss.net>
2018-08-21 13:17:34 +0800

    Add comment for all codes of fake vanilla mode

--- src/main/java/cc/uraniummc/UraniumConfig.java
+++ src/main/java/cc/uraniummc/UraniumConfig.java
@@ -63,7 +63,7 @@ public class UraniumConfig extends ConfigBase {
     public BoolSetting enableGuava17=new BoolSetting(this,"experimental.guava17",false,"EXPERIMENTAL! Using guava17 to replace guava 10 in com.google.common package");
     public StringSetting uraniumName=new StringSetting(this,"experimental.UraniumName","","EXPERIMENTAL! Some plugins not support Uranium as server name, you can change it to KCauldron or Cauldron to improve compatibility");
     public BoolSetting remapReflection=new BoolSetting(this,"experimental.remap-Reflection",false,"EXPERIMENTAL! This options can remap Class.forName getMethod getField to support plugins which using the net.minecraft.server.Rxxx package, but lots of plugin not support this.");
-    public BoolSetting fakeVanillaMode= new BoolSetting(this,"experimental.fakeVanillaMode",false,"EXPERIMENTAL! Make client think this is a Vanilla server.");
+    //public BoolSetting fakeVanillaMode= new BoolSetting(this,"experimental.fakeVanillaMode",false,"EXPERIMENTAL! Make client think this is a Vanilla server.");
     public static boolean tileEntityListRecreation;
     
     public UraniumConfig() {
