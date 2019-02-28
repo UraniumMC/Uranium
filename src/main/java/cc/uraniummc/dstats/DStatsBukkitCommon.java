ceda4fe3c681235531ee488c4b6df7309028a95d
xjboss <i@xjboss.net>
2019-02-28 02:22:29 +0000

    Added DStats

---
+++ src/main/java/cc/uraniummc/dstats/DStatsBukkitCommon.java
@@ -0,0 +1,103 @@
+package cc.uraniummc.dstats;
+
+import cpw.mods.fml.common.event.FMLServerStartedEvent;
+import cpw.mods.fml.common.event.FMLServerStoppedEvent;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import cpw.mods.fml.common.gameevent.PlayerEvent;
+import cpw.mods.fml.common.gameevent.TickEvent;
+import cpw.mods.fml.common.network.NetworkRegistry;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import lombok.*;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.server.MinecraftServer;
+import net.minecraftforge.common.MinecraftForge;
+import org.bukkit.Bukkit;
+import org.spigotmc.SpigotConfig;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+
+import static cc.uraniummc.dstats.DStatsMetricsCommon.AuthMode.BUNGEECORD;
+import static cc.uraniummc.dstats.DStatsMetricsCommon.AuthMode.OFFLINE;
+import static com.google.common.math.DoubleMath.mean;
+
+public abstract class DStatsBukkitCommon {
+    @Getter(AccessLevel.PROTECTED)
+    private Thread thread_dstats;
+    @Getter
+    private DStatsMetricsCommon.DstatsModel stats=new DStatsMetricsCommon.DstatsModel();
+    public abstract double getTPS();
+    protected void loadDStats(){
+        if(Bukkit.getOnlineMode()){
+            stats.setAuthMode(DStatsMetricsCommon.getAuthMode());
+        }else{
+            if(checkIsBungee()){
+                stats.setAuthMode(BUNGEECORD);
+            }else{
+                stats.setAuthMode(OFFLINE);
+            }
+        }
+        parseDetails();
+        findServerDetails();
+        getServerId();
+        val t=new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(60000 * 5); //等待五分钟等插件和模组 加载完成
+                    parseDetails();
+                    while (true) {
+                        getStats().setTps(Double.valueOf(getTPS()).floatValue());
+                        getStats().setOnlinePlayers(Bukkit.getOnlinePlayers().size());
+                        DStatsMetricsCommon.DstatsRetModel ret=DStatsMetricsCommon.sendToServer("https://dstats.xyz/api/v1/ServerSoftwareData/Parse",getStats());
+                        if(ret==null)continue;
+                        switch (ret.getStatus()){
+                            case 1:
+                                updateServerId(UUID.fromString(ret.getExtraDetails()));
+                                break;
+                            default:
+                                //getLogger().info("Remote server result\n Code:"+ret.getStatus()+"\n"+ret.getExtraDetails());
+                                break;
+                        }
+                        Thread.sleep(60000 * 20); //每20分钟发送一次数据
+                    }
+                }catch (Exception e){
+                }
+            }
+        });
+        thread_dstats=t;
+        t.setName("DStats MineCraft Data Center Thread");
+        t.start();
+    }
+    public void parseDetails(){
+        val osarch = System.getProperty("os.arch");
+        val osname = System.getProperty("os.name");
+        val osversion = System.getProperty("os.version");
+        val oscores = Runtime.getRuntime().availableProcessors();
+        val osusemem = (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) / 1024 / 1024;
+        val ostotalmem = Runtime.getRuntime().totalMemory() / 1024 / 1024;
+        val javaversion = System.getProperty("java.version");
+        val javavendor = System.getProperty("java.vendor");
+        getStats().setOS(osname);
+        getStats().setOSPlatform(osarch);
+        getStats().setOSVersion(osversion);
+        getStats().setCore(oscores);
+        getStats().setServerSoftware(Bukkit.getServerName());
+        getStats().setServerVersion(Bukkit.getVersion());
+        getStats().setUseMemory(osusemem);
+        getStats().setTotalMemory(ostotalmem);
+        getStats().setJavaVersion(javaversion);
+        getStats().setJavaVendor(javavendor);
+        findServerDetails();
+        getServerId();
+    }
+    public abstract UUID getServerId();
+    public abstract void updateServerId(UUID uuid);
+    public abstract void findServerDetails();
+    public boolean checkIsBungee(){
+        return SpigotConfig.bungee;
+    }
+}
---
+++ src/main/java/cc/uraniummc/dstats/DStatsUranium.java
@@ -0,0 +1,94 @@
+package cc.uraniummc.dstats;
+
+import cc.uraniummc.Uranium;
+import lombok.Getter;
+import net.minecraft.server.MinecraftServer;
+import net.minecraftforge.common.MinecraftForge;
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
+
+import static com.google.common.math.DoubleMath.mean;
+
+public class DStatsUranium extends DStatsBukkitCommon {
+    private YamlConfiguration dstats_new_config=new YamlConfiguration();
+    private YamlConfiguration dstats_config=new YamlConfiguration();
+    private File dstats_file=new File(MinecraftServer.serverConfigDir,"DStats.yml");
+    private final Object lock =new Object();
+    @Getter
+    private boolean enabled=true;
+    @Getter
+    private UUID guid=UUID.randomUUID();
+    public DStatsUranium() {}
+    public void start(){
+        try {
+            if (!dstats_file.exists()) {
+                saveConfig();
+            }else{
+                loadConfig();
+            }
+        }catch (IOException e){
+            try {
+                guid=UUID.randomUUID();
+                saveConfig();
+            }catch (IOException ee){}
+        }
+    }
+    public void restart(){
+        try {
+            getThread_dstats().interrupt();
+            getThread_dstats().wait();
+        }catch (Exception e){}
+        start();
+    }
+    public void loadConfig() throws IOException {
+        synchronized (lock) {
+            try {
+                dstats_new_config.load(dstats_file);
+            } catch (InvalidConfigurationException e) {
+                throw new IOException(e);
+            }
+            dstats_config=dstats_new_config;
+            enabled = dstats_config.getBoolean("Enable",enabled);
+            guid = UUID.fromString(dstats_config.getString("Guid",guid.toString()));
+        }
+    }
+    public void saveConfig() throws IOException{
+        synchronized (lock){
+            dstats_config.set("Enable",enabled);
+            dstats_config.set("Guid",guid.toString());
+            dstats_config.save(dstats_file);
+        }
+    }
+    @Override
+    public double getTPS() {
+        final MinecraftServer server = MinecraftServer.getServer();
+        double meanTickTime = mean(server.tickTimeArray) * 1.0E-6D;
+        return Math.min(1000.0 / meanTickTime, 20);
+    }
+
+    @Override
+    public UUID getServerId() {
+        return guid;
+    }
+
+    @Override
+    public void updateServerId(UUID uuid) {
+        guid=uuid;
+        try {
+            saveConfig();
+        }catch (IOException e){
+
+        }
+    }
+
+    @Override
+    public void findServerDetails() {
+        getStats().setMinecraftVersion(MinecraftForge.MC_VERSION);
+        getStats().setServerSoftware(Uranium.name);
+        getStats().setServerVersion(Uranium.getCurrentVersion());
+    }
+}
