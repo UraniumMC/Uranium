68898586395538baf188af6c1a142c9f1a0e48fd
xjboss <i@xjboss.net>
2020-03-02 13:56:50 +0800

    借鉴CatServer的preventBlockLoadChunk选项

--- src/main/java/cc/uraniummc/UraniumConfig.java
+++ src/main/java/cc/uraniummc/UraniumConfig.java
@@ -84,6 +84,8 @@ public class UraniumConfig extends ConfigBase {
     public BoolSetting enableForgeRespawnClone =new BoolSetting(this,"forge.clone-player-on-respawn",false,"Original forge will clone a new player when player respawn, some mods needs this feature.");
     public BoolSetting enableEndlessLoopCheck =new BoolSetting(this,"EndlessLoopCheck.enable",false,"enable a check to prevent endless loop");
     public IntSetting endlessLoopCheckDeep =new IntSetting(this,"EndlessLoopCheck.deep",2,"same event type,default loop deep");
+    public BoolSetting enablePreventChunkLoad = new BoolSetting(this, "experimental.PerventChunkLoad",false,"EXPERIMENTAL ! Prevent tile to load chunk, it will borken lots mods," +
+            "but it maybe can prevent server crash!");
     public UraniumConfig() {
         super("uranium.yml", "um");
         for(Field sField : UraniumConfig.class.getDeclaredFields()){
